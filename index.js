10c951c31ee10d3441580f37cc8278152f58ba08
unknown <braxtonsislo@gmail.com>
2021-05-05 20:45:16 -0500

    didmybestlol

--- index.js
+++ index.js
@@ -28,11 +28,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+    -counter 2 has the global variable while counter 1 has a scoped variable
   2. Which of the two uses a closure? How can you tell?
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+     counter 2 would be better when you want to have the variable delcared outside of the function.
 */
 
 // counter1 code
@@ -62,8 +63,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(num){
+  return Math.floor(Math.random() * Math.floor(2));
 }
 
 
@@ -81,7 +82,7 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(scoreCB,){
   /*Code Here*/
 }
 
@@ -90,7 +91,7 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
+function getInningScore(inningCB) {
   /*Your Code Here */
 }
 
